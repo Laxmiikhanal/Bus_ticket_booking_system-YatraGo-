13adbb615ac448167827538efee7ace4e89d5d1e
Laxmi Khanal <pujakhanal633@gmail.com>
2026-06-08 14:36:08 +0545

    feat: add YatraGo color palette

---
+++ lib/core/theme/app_colors.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color darkGreen  = Color(0xFF0A3D1F);
+  static const Color primary    = Color(0xFF16A34A);
+  static const Color light      = Color(0xFF22C55E);
+  static const Color pale       = Color(0xFF4ADE80);
+  static const Color orange     = Color(0xFFF97316);
+  static const Color orangeDark = Color(0xFFEA580C);
+  static const Color background = Color(0xFFF0EDE8);
+  static const Color white      = Color(0xFFFFFFFF);
+  static const Color cardBg     = Color(0xFFFDFCFB);
+  static const Color textDark   = Color(0xFF0A1A10);
+  static const Color textGrey   = Color(0xFF9CA3AF);
+  static const Color textLight  = Color(0xFFB0B8C1);
+  static const Color success    = Color(0xFF22C55E);
+  static const Color warning    = Color(0xFFF97316);
+  static const Color error      = Color(0xFFDC2626);
+  static const Color info       = Color(0xFF3B82F6);
+  static const Color seatAvailable = Color(0xFFF3F4F6);
+  static const Color seatSelected  = Color(0xFF22C55E);
+  static const Color seatBooked    = Color(0xFFFECACA);
+  static const Color seatWomen     = Color(0xFFFED7AA);
+}
